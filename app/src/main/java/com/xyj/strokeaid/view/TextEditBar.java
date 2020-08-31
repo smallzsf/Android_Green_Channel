10295b4ba244421c38fd9468844dabd286508b7b
lichengyang <licy3051@qq.com>
2020-08-31 10:33:46 +0800

    feat:  新增患者页面修改

---
+++ app/src/main/java/com/xyj/strokeaid/view/TextEditBar.java
@@ -0,0 +1,23 @@
+package com.xyj.strokeaid.view;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * TextEditBar
+ * description: TODO
+ *
+ * @author : Licy
+ * @date : 2020/8/30
+ * email ：licy3051@qq.com
+ */
+public class TextEditBar extends LinearLayout {
+    public TextEditBar(Context context) {
+        super(context);
+    }
+}
+
+    
+    
+       
+    
