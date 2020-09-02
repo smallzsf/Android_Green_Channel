65b13bd9741f44a78db187f493b2df05af82d59d
lichengyang <licy3051@qq.com>
2020-09-02 10:05:18 +0800

    feat:  页面框架调整

---
+++ app/src/main/java/com/xyj/strokeaid/activity/chestpain/ChestPainEmptyActivity.java
@@ -0,0 +1,16 @@
+package com.xyj.strokeaid.activity.chestpain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xyj.strokeaid.R;
+
+public class ChestPainEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chest_pain_empty);
+    }
+}
