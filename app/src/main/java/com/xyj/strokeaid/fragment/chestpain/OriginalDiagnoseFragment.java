e4d6259d3927a2122f9c2c961d8140e06eb83f6d
xiaohei <2411320454@qq.com>
2020-09-02 19:16:08 +0800

    新增 初始诊断  患者转归 xml文件

---
+++ app/src/main/java/com/xyj/strokeaid/fragment/chestpain/OriginalDiagnoseFragment.java
@@ -0,0 +1,30 @@
+package com.xyj.strokeaid.fragment.chestpain;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.xyj.strokeaid.base.BaseFragment;
+
+/**
+ * @ClassName: OriginalDiagnoseFragment
+ * @Description:
+ * @Author: 小黑
+ * @Date: 2020/9/2 19:09
+ */
+public class OriginalDiagnoseFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(@NonNull View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
---
+++ app/src/main/java/com/xyj/strokeaid/fragment/chestpain/PatientSumFragment.java
@@ -0,0 +1,30 @@
+package com.xyj.strokeaid.fragment.chestpain;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.xyj.strokeaid.base.BaseFragment;
+
+/**
+ * @ClassName: PatientSumFragment
+ * @Description:
+ * @Author: 小黑
+ * @Date: 2020/9/2 19:10
+ */
+public class PatientSumFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(@NonNull View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
