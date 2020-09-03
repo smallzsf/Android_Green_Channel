c49dac34a5fc194813a6eb59433dae42d942b7a8
lt0304 <122724021@qq.com>
2020-09-03 17:24:31 +0800

    手术治疗--结果信息完成--刘廷--9.3

---
+++ app/src/main/java/com/xyj/strokeaid/bean/GenderSelectBean.java
@@ -0,0 +1,38 @@
+package com.xyj.strokeaid.bean;
+
+public class GenderSelectBean {
+
+    private int position;
+    private int type;   //0-第一个集合  1-更多集合
+    private String name;
+
+    public GenderSelectBean(int position, int type, String name) {
+        this.position = position;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
