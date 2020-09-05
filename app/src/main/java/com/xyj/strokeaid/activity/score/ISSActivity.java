645074af7487e668121d68113e2a43899794aab4
van <212333519@qq.com>
2020-09-05 19:27:25 +0800

    提交

--- app/src/main/java/com/xyj/strokeaid/activity/score/ISSActivity.java
+++ app/src/main/java/com/xyj/strokeaid/activity/score/ISSActivity.java
@@ -1,4 +1,53 @@
 package com.xyj.strokeaid.activity.score;
 
-public class ISSActivity {
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.xyj.strokeaid.R;
+import com.xyj.strokeaid.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ISSActivity extends BaseActivity {
+
+    @BindView(R.id.listview)
+    ListView listview;
+
+    private List<String[]> dataList = new ArrayList<>();
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_score_iss;
+    }
+
+    @Override
+    protected void initInject() {
+
+//        listview.setAdapter(new MyAdapter());
+        initData();
+    }
+
+    private void initData() {
+        dataList = ISSCommon.getData();
+
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+
+
+
 }
