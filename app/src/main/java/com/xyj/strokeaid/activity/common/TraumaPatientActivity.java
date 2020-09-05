21718eaa4f43e0924f17db8dd3b1fed466a5d9a9
zhangshifu <zhangshifu@mtsports.com.cn>
2020-09-05 10:21:33 +0800

    提交定义fragment

--- app/src/main/java/com/xyj/strokeaid/activity/common/TraumaPatientActivity.java
+++ app/src/main/java/com/xyj/strokeaid/activity/common/TraumaPatientActivity.java
@@ -175,6 +175,29 @@ public class TraumaPatientActivity extends BaseActivity {
         public Fragment createFragment(int position) {
             switch (position) {
                 case 0:
+
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
                 default:
                     return EmptyFragment.newInstance();
             }
