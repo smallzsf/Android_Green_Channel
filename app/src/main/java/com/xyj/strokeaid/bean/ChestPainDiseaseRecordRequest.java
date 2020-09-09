e90031e98d3b4232c7eafd467307dcb27dcc1c57
cairuiqi <1461788863@qq.com>
2020-09-09 10:31:00 +0800

    病情记录保存、查询对接

---
+++ app/src/main/java/com/xyj/strokeaid/bean/ChestPainDiseaseRecordRequest.java
@@ -0,0 +1,62 @@
+package com.xyj.strokeaid.bean;
+
+/**
+ * @Description: 胸痛病情记录
+ * @Author: crq
+ * @CreateDate: 2020/9/9 9:36
+ */
+public class ChestPainDiseaseRecordRequest {
+    /**
+     * recordId : 751546854738628608
+     * conditionassessment : cpc_bqpg_cxxxm
+     * conditionassessmentdetail : cpc_bqpgmx_hxkn
+     * chiefcomplaint : 1
+     * conditionassessmentremark : 2
+     */
+
+    private String recordId;
+    private String conditionassessment;
+    private String conditionassessmentdetail;
+    private String chiefcomplaint;
+    private String conditionassessmentremark;
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
+
+    public String getConditionassessment() {
+        return conditionassessment;
+    }
+
+    public void setConditionassessment(String conditionassessment) {
+        this.conditionassessment = conditionassessment;
+    }
+
+    public String getConditionassessmentdetail() {
+        return conditionassessmentdetail;
+    }
+
+    public void setConditionassessmentdetail(String conditionassessmentdetail) {
+        this.conditionassessmentdetail = conditionassessmentdetail;
+    }
+
+    public String getChiefcomplaint() {
+        return chiefcomplaint;
+    }
+
+    public void setChiefcomplaint(String chiefcomplaint) {
+        this.chiefcomplaint = chiefcomplaint;
+    }
+
+    public String getConditionassessmentremark() {
+        return conditionassessmentremark;
+    }
+
+    public void setConditionassessmentremark(String conditionassessmentremark) {
+        this.conditionassessmentremark = conditionassessmentremark;
+    }
+}
